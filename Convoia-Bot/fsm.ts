2f50f496ddca98680d2189d6e79b76e8f1620cae
Meet <meetaparekh2003@gmail.com>
2023-12-09 12:31:44 +0530

    fsm updation 2

--- Convoia-Bot/fsm.ts
+++ Convoia-Bot/fsm.ts
@@ -3,14 +3,21 @@ import { t, StateMachine, ITransition } from "typescript-fsm";
 import { AskForCollection, AskForEvent, WelcomeMessage } from "./messages.js";
 // query file needed
 import { GetPOAPEvent, GetTokenHoldersByTokenAddress, checkCollectionExists, checkPoapEventExistence, getPoapEventInfo } from "./query.js";
-//sendNFTData.js file needed 
 import { sendImage, sendTokenInfo } from "./sendNFTData.js";
+//sendNFTData.js file needed 
 
 
-
+enum state{}
+enum Events{}
 
 // created dispatch var things imported from message ,query , sendNFTData need to be created 
 export const dispatch = async (context: any)=>{
+    const messageBody = context.message.content;
+    //geting state and event from fsm
+    const state = fsm.getState();
+    //state will come from enum
+    console.log("state", state)
+
 
 
 }

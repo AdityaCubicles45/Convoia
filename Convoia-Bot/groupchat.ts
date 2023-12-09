99c7d4bed491d9db8e8d04de3002f622c57499da
Meet <meetaparekh2003@gmail.com>
2023-12-09 21:11:02 +0530

    groupchat.ts updation 1 creating wallet var

--- Convoia-Bot/groupchat.ts
+++ Convoia-Bot/groupchat.ts
@@ -3,3 +3,9 @@ import { Client } from '@xmtp/xmtp-js';
 import { Wallet, type HDNodeWallet } from 'ethers';
 //@ts-ignore
 import qrcode from 'qrcode-terminal'
+
+
+let wallet: HDNodeWallet | null = null;
+let xmtp: Client;
+const WALLET_TO = "0x93e2fc3e99dfb1238eb9e0ef2580efc5809c7204";
+let conversation: any;

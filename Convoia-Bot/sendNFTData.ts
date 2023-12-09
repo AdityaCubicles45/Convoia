b54c276e6af4289f83e983881cda887ddb0471dc
Meet <meetaparekh2003@gmail.com>
2023-12-09 18:53:43 +0530

    sendNFTData.ts file updation 1

--- Convoia-Bot/sendNFTData.ts
+++ Convoia-Bot/sendNFTData.ts
@@ -6,3 +6,11 @@ import Upload from './uploadFile.js';
 import axios from 'axios'
 import HandlerContext from "@xmtp/bot-starter/dist/HandlerContext.js";
 import 'dotenv/config'
+
+
+
+export const sendTokenInfo = async (context: any, data: any) => {
+	//filter out tokens whose user has no XMTP
+	const ethToken = data.ethereum.TokenBalance
+	const polToken = data.polygon.TokenBalance
+}

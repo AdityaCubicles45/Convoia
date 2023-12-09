46dc034ecea61ebcfcf933b2ce15b379ea403315
Meet <meetaparekh2003@gmail.com>
2023-12-09 11:09:33 +0530

    fsm updation 1

--- Convoia-Bot/fsm.ts
+++ Convoia-Bot/fsm.ts
@@ -1,4 +1,16 @@
 import { t, StateMachine, ITransition } from "typescript-fsm";
+//message file needed 
 import { AskForCollection, AskForEvent, WelcomeMessage } from "./messages.js";
+// query file needed
 import { GetPOAPEvent, GetTokenHoldersByTokenAddress, checkCollectionExists, checkPoapEventExistence, getPoapEventInfo } from "./query.js";
+//sendNFTData.js file needed 
 import { sendImage, sendTokenInfo } from "./sendNFTData.js";
+
+
+
+
+// created dispatch var things imported from message ,query , sendNFTData need to be created 
+export const dispatch = async (context: any)=>{
+
+
+}

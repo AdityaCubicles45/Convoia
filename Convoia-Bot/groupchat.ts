7034f9acbbb4b60ff0c037aad2807115b7fd2bce
Meet <meetaparekh2003@gmail.com>
2023-12-09 23:04:41 +0530

    groupchat updation adding key

--- Convoia-Bot/groupchat.ts
+++ Convoia-Bot/groupchat.ts
@@ -3,3 +3,8 @@ import { AttachmentCodec, RemoteAttachmentCodec } from '@xmtp/content-type-remot
 import { Client } from '@xmtp/xmtp-js';
 import 'dotenv/config'
 import { Wallet, getDefaultProvider } from 'ethers';
+
+process.env.KEY = process.env.GROUPCHAT_KEY|| ""
+
+console.log("process.env.KEY", process.env.KEY)
+const wallet = new Wallet(process.env.KEY);

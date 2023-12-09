4262cbd3635eb69c127b55fbe25dfa0f47182fc4
Meet <meetaparekh2003@gmail.com>
2023-12-09 10:53:29 +0530

    functionality of bot.ts

--- Convoia-Bot/bot.ts
+++ Convoia-Bot/bot.ts
@@ -6,4 +6,12 @@ import { dispatch } from './fsm.js'
 import HandlerContext from '@xmtp/bot-starter/dist/HandlerContext.js';
 import 'dotenv/config.js'
 
-run(async()=>{}).catch((err)=>{});
+run(async(context:HandlerContext)=>{
+    //HandlerContext help bot to decode 
+    // the message sended by user
+    console.log("Loading........");
+    await dispatch (context);
+    console.log("dispatched")
+}).catch((err)=>{
+    console.error(err);
+});

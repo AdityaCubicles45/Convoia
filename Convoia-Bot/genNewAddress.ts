8e7aa2e9d74c6401a048e902f00dd82a86333172
Meet <meetaparekh2003@gmail.com>
2023-12-09 23:09:29 +0530

    genNewAddress.ts Final updation

--- Convoia-Bot/genNewAddress.ts
+++ Convoia-Bot/genNewAddress.ts
@@ -1,1 +1,5 @@
 import { Wallet } from "ethers";
+
+const randomWallet = Wallet.createRandom();
+const privateKey: string = randomWallet.privateKey;
+console.log("Set your environment variable: KEY=" + privateKey);

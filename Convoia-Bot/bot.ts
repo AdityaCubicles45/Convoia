e2c79d8cbdc6ae4336ade31cff1d0ca3be4221d6
Meet <meetaparekh2003@gmail.com>
2023-12-09 10:46:15 +0530

    Initial creating bot.ts file

---
+++ Convoia-Bot/bot.ts
@@ -0,0 +1,9 @@
+import run from '@xmtp/bot-starter'
+
+//@ts-ignore
+import qrcode from 'qrcode-terminal'
+import { dispatch } from './fsm.js'
+import HandlerContext from '@xmtp/bot-starter/dist/HandlerContext.js';
+import 'dotenv/config.js'
+
+run(async()=>{}).catch((err)=>{});

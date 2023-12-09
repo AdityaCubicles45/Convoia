06223aab352dc2847d8db114137fa65863e5d371
Meet <meetaparekh2003@gmail.com>
2023-12-09 19:14:08 +0530

    uploadFile.ts upadtion 1

--- Convoia-Bot/uploadFile.ts
+++ Convoia-Bot/uploadFile.ts
@@ -1,1 +1,14 @@
 import { Filelike } from "web3.storage";
+
+export default class Upload implements Filelike {
+    name: string;
+    data: Uint8Array;
+  
+    constructor(name: string, data: Uint8Array) {
+      this.name = name;
+      this.data = data;
+    }
+    stream: () => ReadableStream{
+        
+    };
+}

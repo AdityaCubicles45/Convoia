e811b006e6b0f7727c4eee9bbb947f1df404d560
Meet <meetaparekh2003@gmail.com>
2023-12-09 20:42:38 +0530

    fsm.ts final updation

--- Convoia-Bot/fsm.ts
+++ Convoia-Bot/fsm.ts
@@ -1,10 +1,7 @@
 import { t, StateMachine, ITransition } from "typescript-fsm";
-//message file needed 
 import { AskForCollection, AskForEvent, WelcomeMessage } from "./messages.js";
-// query file needed
 import { GetPOAPEvent, GetTokenHoldersByTokenAddress, checkCollectionExists, checkPoapEventExistence, getPoapEventInfo } from "./query.js";
 import { sendImage, sendTokenInfo } from "./sendNFTData.js";
-//sendNFTData.js file needed 
 
 enum States { waitingForUser = 0, waitingFirstInput, waitingForEventName ,waitingForCollectionName,waitingForGroupChatConfirmation,fetchingCollection};
 enum Events { userLogin = 100, wrongInput, sendNFT, sendPOAP , backToTop, proposeGroupChat , retry , sendCollectionName , };
@@ -52,9 +49,7 @@ const fsm = new StateMachine<States,Events>(
 // created dispatch var things imported from message ,query , sendNFTData need to be created 
 export const dispatch = async (context: any)=>{
     const messageBody = context.message.content;
-    //geting state and event from fsm
     const state = fsm.getState();
-    //state will come from enum
     console.log("state", state)
     switch(state){
         case States.waitingForUser:

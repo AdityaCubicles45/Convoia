a6ae9e8e2b9c7b75f2e217ee6d4db495011af2e1
Meet <meetaparekh2003@gmail.com>
2023-12-09 20:40:03 +0530

    message.ts final updation

--- Convoia-Bot/messages.ts
+++ Convoia-Bot/messages.ts
@@ -1,2 +1,5 @@
 export const AskForEvent = "Please type the name of the POAP you are looking for"
 export const AskForCollection = "Please type the name of the collection you are looking for"
+export const WelcomeMessage = "Welcome to the Token Terminal GraphQL API! \n \
+  I am here to help you find NFTs and contact their owners. \n \
+  Type NFT to look at NFTs collection, or type EVENT to look at events. \n"

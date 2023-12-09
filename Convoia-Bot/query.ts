c523723fd1cea66ba7ab68b7715fe290275859d3
Meet <meetaparekh2003@gmail.com>
2023-12-09 15:05:52 +0530

    query.ts component updation 1

--- Convoia-Bot/query.ts
+++ Convoia-Bot/query.ts
@@ -1,2 +1,26 @@
 import { gql } from "graphql-request";
 import { request} from 'graphql-request'
+
+// Query to get the holders of tokens in a collection
+export const GetTokenHolders = `gql`
+
+
+
+
+
+
+
+
+
+
+
+export const GetTokenHoldersByTokenAddress = async (tokenAddress: string) =>  {
+    const data = await request('https://api.airstack.xyz/gql', GetTokenHolders, { tokenAddress: tokenAddress, limit: 100, headers: {
+      authorization: `Bearer MY_TOKEN`,
+    }})
+  
+    return data;
+  
+  }
+
+  
